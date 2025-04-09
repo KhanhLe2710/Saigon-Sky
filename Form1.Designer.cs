551aef5699873d2b9a76c85a16d3cd3b42bed834
KhanhLe2710 <leducbaokhanh2710@gmail.com>
2025-04-09 20:49:16 +0700

    Tạo giao diện chọn khu vực và khung hiển thị dự báo thời tiết

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             button1 = new Button();
             checkedListBox1 = new CheckedListBox();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -49,11 +50,21 @@
             checkedListBox1.Size = new Size(150, 114);
             checkedListBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            button2.Location = new Point(497, 99);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(checkedListBox1);
             Controls.Add(button1);
             Name = "Form1";
@@ -65,5 +76,6 @@
 
         private Button button1;
         private CheckedListBox checkedListBox1;
+        private Button button2;
     }
 }
